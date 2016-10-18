fcb0e2461d3bd31dc9e9f5249fcd78c3ef98dd15
willowrose <willowrose@users.noreply.github.com>
2016-10-19 00:00:43 +0200

    Increase default max transaction fee

--- src/wallet/wallet.h
+++ src/wallet/wallet.h
@@ -43,7 +43,7 @@ static const CAmount DEFAULT_TRANSACTION_FEE = 0;
 //! -paytxfee will warn if called with a higher fee than this amount (in satoshis) per KB
 static const CAmount nHighTransactionFeeWarning = 0.01 * COIN;
 //! -maxtxfee default
-static const CAmount DEFAULT_TRANSACTION_MAXFEE = 0.1 * COIN;
+static const CAmount DEFAULT_TRANSACTION_MAXFEE = 10 * COIN;
 //! -txconfirmtarget default
 static const unsigned int DEFAULT_TX_CONFIRM_TARGET = 2;
 //! -maxtxfee will warn if called with a higher fee than this amount (in satoshis)
